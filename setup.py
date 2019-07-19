e05de7db9c27985846ce40c64ed4348b8a556295
jianboli <KNSNus39>
2019-07-19 10:24:50 -0400

    updated the version

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 setup(
-    name='pyfolio-script',    # This is the name of your PyPI-package.
-    version='0.1',                          # Update the version number for new releases
-    scripts=['pyfolio'],                  # The name of your scipt, and also the command you'll be using for calling it
-    install_requires=[
+    name='pyfolio',    # This is the name of your PyPI-package.
+    version='0.2',                          # Update the version number for new releases
+    url='https://github.com/jianboli/pyfolio',                  # The name of your scipt, and also the command you'll be using for calling it
+    author="Jianbo Li",
+	license="MIT",
+	install_requires=[
         'numpy', 
         'pandas', 
         'tia'
