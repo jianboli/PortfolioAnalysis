a43b4b559063341017213b9a9d8d48f2dc340c3c
jianboli <KNSNus39>
2019-07-22 11:30:18 -0400

    1. Some functions is not exported

--- pyfolio/__init__.py
+++ pyfolio/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 
-from .paper_portfolio import get_period_return
+from .paper_portfolio import get_period_return, get_security_hist, get_daily_return, AdjMode, Periodicity
 from .index_data import get_memb
 from .portfolio import statistics, mdd, format_pnl, cumpound_pnl
