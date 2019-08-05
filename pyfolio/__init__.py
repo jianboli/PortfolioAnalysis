f2cd81602764ee8b3cb1a0e3c803f9bad85c9ca8
jianboli <jli@threebridgescap.com>
2019-08-05 15:11:02 -0400

    1. Added the net fee calcuation

--- pyfolio/__init__.py
+++ pyfolio/__init__.py
@@ -2,4 +2,4 @@
 
 from .paper_portfolio import get_period_return, get_security_hist, get_daily_return, AdjMode, Periodicity
 from .index_data import get_memb
-from .portfolio import statistics, mdd, format_pnl, cumpound_pnl
+from .portfolio import statistics, mdd, format_pnl, cumpound_pnl, calc_net_performance
--- pyfolio/portfolio.py
+++ pyfolio/portfolio.py
@@ -30,6 +30,61 @@ def mdd(r):
     dd = -(cum/peak-1)
     return dd.max()
 
+def calc_net_performance(gross_perf, management_fee, incentive_fee):
+    """
+    Calucate monthly net after incentive_fee and management_fee. Most common configuration:
+    - high water mark, no clawbacks
+    - incentive fee is applied on the performance (no benchmark based performance)
+    :param gross_perf: the pandas data series/table contains monthly gross performance with monthly end date as index
+    :param management_fee: management fee ratio
+    :param incentive_fee: incentive fee
+    :return: the net performance as data Series after all the fees
+    """
+    # make sure the gross is a series
+    if isinstance(gross_perf, pd.DataFrame):
+        gross_perf = gross_perf.iloc[:, 0]
+
+    # make sure the index is datetime
+    gross_perf.index = pd.to_datetime(gross_perf.index)
+    gross_perf.sort_index()
+    prev_months_cumulative = 0
+    prev_ytd = 0
+    dec_ytd = 0
+    high_water_mark = 0
+
+    net = list()
+    for dt, gr in gross_perf.iteritems():
+        new_months_cumulative = (gr + 1) * (prev_months_cumulative + 1) - 1
+        management_fee_month = (prev_months_cumulative + 1) * (management_fee / 12)
+
+        if dt.month == 1:
+            dec_ytd = prev_months_cumulative
+            if dec_ytd > high_water_mark:
+                high_water_mark = dec_ytd
+
+        new_cummulative_after_management = new_months_cumulative - management_fee_month
+
+        # Calculated YTD Management for YTDPerformance Calculation
+        ytd_managemnt = (new_cummulative_after_management + 1) / (dec_ytd + 1) - 1
+
+        # Only charge performance when (new_cummulative_after_management - high_water_mark) > 0 i.e WaterMark exceed previous year
+        if incentive_fee > 0 and new_cummulative_after_management > high_water_mark:
+            ytd_performance = ytd_managemnt - (incentive_fee * (1 + ytd_managemnt) * ((new_cummulative_after_management - high_water_mark) / (1 + new_cummulative_after_management)))
+        else:
+            ytd_performance = ytd_managemnt
+
+        if dt.month == 1:
+            monthly_net = ytd_performance
+        else:
+            monthly_net = (1 + ytd_performance) / (1 + prev_ytd) - 1
+
+        net.append(monthly_net)
+        prev_months_cumulative = new_cummulative_after_management
+        prev_ytd = ytd_performance
+
+    return pd.Series(net, index=gross_perf.index)
+
+
 def statistics(r, b=None, rf=None, freq=12):
     """
     Generate the portfolio return matrix from the monthly P&L including the following:
@@ -77,3 +132,22 @@ def statistics(r, b=None, rf=None, freq=12):
 
     df = pd.DataFrame(stat, columns=['Summary', 'Stat'])
     return df
+
+
+if __name__ == "__main__":
+    test_net_fee = True
+    if test_net_fee:
+        gross = pd.DataFrame(data = [0.034107741833528, -0.00833852885640296, 0.0320612695430027, 0.0102631014311665, 0.0113370205823213, 0.0180773838954622,
+                            0.047031427894648, 0.00295545925707486, 0.0324885986089885, 0.00865585089499699, 0.00346117358058629, 0.0081528599398073, 0.0148837779649957,
+                            -0.00418473351008752, 0.056113255346828, 0.0205687580374805, -0.0142217587735437, 0.00766906087868691, 0.0235982274548432, -0.00915881701770105,
+                            0.0139498718873998, 0.00555486136711258,  0.0066962328677207, -0.000910732639986089, -0.00497602777657502, 0.00174304734261033, 0.00619146958299455,
+                            0.024184586, 0.036688053066313, 0.0115983334056298, 0.0214857037577827, 0.00458691255044807, 0.00655048461181207],
+                           index=["30-Nov-2016", "30-Dec-2016", "31-Jan-2017", "28-Feb-2017", "31-Mar-2017", "28-Apr-2017", "31-May-2017", "30-Jun-2017", "31-Jul-2017", "31-Aug-2017",
+                                  "29-Sep-2017", "31-Oct-2017", "30-Nov-2017", "29-Dec-2017", "31-Jan-2018", "28-Feb-2018", "31-Mar-2018", "30-Apr-2018", "31-May-2018", "30-Jun-2018", "31-Jul-2018", "31-Aug-2018",
+                                  "30-Sep-2018", "31-Oct-2018", "30-Nov-2018", "31-Dec-2018", "31-Jan-2019", "28-Feb-2019", "31-Mar-2019", "30-Apr-2019", "31-May-2019", "30-Jun-2019", "31-Jul-2019"])
+        gross.index = pd.to_datetime(gross.index, format="%d-%b-%Y")
+        net = calc_net_performance(gross, 0.01, 0.1)
+        print("Cumulative Net: {}".format((net+1).product() - 1))
+        assert np.abs((net+1).product() - 1 -  0.429888256) < 1e-6, "The Net fee calcuation does not look right!"
+
+        pass
